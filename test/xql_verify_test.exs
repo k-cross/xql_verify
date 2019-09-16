810dba8c8516c03331523ef5460f2061bac67b37
Kenneth Cross <ken.cross@dropbox.com>
2019-09-16 14:35:21 -0700

    removed length constraint

--- test/xql_verify_test.exs
+++ test/xql_verify_test.exs
@@ -5,7 +5,7 @@ defmodule XqlVerifyTest do
   import StreamData
 
   property "passes all valid coordinates" do
-    check all foo_map <- map_of(string(:printable), one_of([json_valid(), list_of(json_valid())]), min_length: 500), max_runs: 5_000 do
+    check all foo_map <- map_of(string(:printable), one_of([json_valid(), list_of(json_valid())])), max_runs: 5_000 do
     #check all foo_map <- map_of(string(:printable), string(:printable)), max_runs: 5_000 do
       foo = %XqlVerify.Foo{bar: foo_map}
       {:ok, _} = XqlVerify.Repo.insert(foo)
